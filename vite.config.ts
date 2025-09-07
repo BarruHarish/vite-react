b3f062c47a6dcfc930d2fbac6f8d162d831ec6a0
B.HARISH <bharish1214@gmail.com>
2025-09-07 17:32:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
-{
-  "compilerOptions": {
-    "target": "ESNext",
-    "useDefineForClassFields": true,
-    "lib": ["DOM", "DOM.Iterable", "ESNext"],
-    "allowJs": false,
-    "skipLibCheck": true,
-    "esModuleInterop": false,
-    "allowSyntheticDefaultImports": true,
-    "strict": true,
-    "forceConsistentCasingInFileNames": true,
-    "module": "ESNext",
-    "moduleResolution": "Node",
-    "resolveJsonModule": true,
-    "isolatedModules": true,
-    "noEmit": true,
-    "jsx": "react-jsx",
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-    "noUnusedLocals": false,
-    "noUnusedParameters": false
-  },
-  "include": ["src"]
-}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
